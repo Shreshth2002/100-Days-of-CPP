0bdd8251ac2d48541e10c414e7cf0cc36e4f40db
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-13 20:39:07 +0530

    Day-5

---
+++ Conditional Statements/Switch Case/VowelConsonent.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    char alphabet;
+
+    cout<<"Enter any alphabet"<<endl;
+    cin>>alphabet;
+
+    switch(alphabet)
+    {
+        case 'a':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'e':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'i':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'o':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'u':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'A':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'E':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'I':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'O':
+        cout<<"It is a vowel";
+        break; 
+
+        case 'U':
+        cout<<"It is a vowel";
+        break; 
+
+        defaut:
+        cout<<"It is a consonent";
+    }
+
+    return 0;
+}
+
+
+
