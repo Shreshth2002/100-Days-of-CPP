bd6e2973f40198f772c2e2d57f644652072ada67
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-14 15:44:51 +0530

    Day-6

---
+++ Loops/ForLoop/prime.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number,factors=0;
+
+    cout<<"Enter the number that you want to check"<<endl;
+    cin>>number;
+
+    for(int i=1; i<=number; i++)
+    {
+        if(number%i==0)
+        {
+        factors++;
+        }
+    }
+
+    if(factors==2)
+    {
+        cout<<"Entered number is a prime number";
+    }
+
+    else
+    {
+        cout<<"Entered number is not a prime number";
+    }
+
+    return 0;
+}
