8e39452d58e6c07800ad7ec627743e9be27866c4
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-18 18:20:11 +0530

    Day-10

---
+++ Loops/WhileLoop/1stProgram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    // initialization expression
+    int i = 1;
+  
+    // test expression
+    while (i <= 10) {
+        cout <<i<<" ";
+  
+        // update expression
+        i++;
+    }
+  
+    return 0;
+}
