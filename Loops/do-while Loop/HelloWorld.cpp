94ece06f9ebbb55750ef3ec6c055c4294d882e42
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-21 23:31:32 +0530

    Day-13

---
+++ Loops/do-while Loop/HelloWorld.cpp
@@ -0,0 +1,23 @@
+// C++ program to illustrate do-while loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Initialization expression
+	int i = 2;
+
+	do {
+		// Loop body
+		cout << "Hello World\n";
+
+		// Update expression
+		i++;
+
+	}
+	// Test expression
+	while (i < 1);
+
+	return 0;
+}
