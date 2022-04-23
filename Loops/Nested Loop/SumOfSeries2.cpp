4767761ad33e9061e81b220336857e7e0ebd8ddb
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-23 23:01:05 +0530

    Day-15

--- Loops/Nested Loop/SumOfSeries2.cpp
+++ Loops/Nested Loop/SumOfSeries2.cpp
@@ -9,10 +9,10 @@ int main()
 	int i,j,n,fact,sign=-1;
     float x, p,sum=0;
 
-	cout<<"Enter the value of x : ";
+	cout<<"Enter the value of x"<<endl;
     cin>>x;
 
-	cout<<"Enter the value of n : ";
+	cout<<"Enter the value of n"<<endl;
     cin>>n;
     
     for(i=2;i<=n;i+=2)
