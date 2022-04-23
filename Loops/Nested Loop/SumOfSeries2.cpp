39edfd75aac5e3b355157428d549696bd22034ca
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-23 22:34:29 +0530

    Day-15

---
+++ Loops/Nested Loop/SumOfSeries2.cpp
@@ -0,0 +1,36 @@
+//cos x = 1 - x2/2! + x4/4! - x6/6! -.....
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int i,j,n,fact,sign=-1;
+    float x, p,sum=0;
+
+	cout<<"Enter the value of x : ";
+    cin>>x;
+
+	cout<<"Enter the value of n : ";
+    cin>>n;
+    
+    for(i=2;i<=n;i+=2)
+    {
+        p=1;
+		fact=1;
+
+		for(j=1;j<=i;j++)
+        {
+            p=p*x;
+            fact=fact*j;
+        }
+
+		sum+=sign*p/fact;
+        sign=-1*sign;
+        }
+
+	cout<<"cos "<<x<<"="<<1+sum;
+    return 0;
+
+}
