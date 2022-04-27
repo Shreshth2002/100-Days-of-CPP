744a311bec5f03a36de3a9511f3efd70db8c9e87
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-27 19:51:17 +0530

    Day-19

---
+++ Patterns/8.cpp
@@ -0,0 +1,34 @@
+/*
+
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int n, s, i, j;
+cout << "Enter number of rows: ";
+cin >> n;
+for(i = 1; i <= n; i++)
+{
+//for loop for displaying space
+for(s = i; s < n; s++)
+{
+cout << " ";
+}
+//for loop to display star equal to row number
+for(j = 1; j <= i; j++)
+{
+cout << "* ";
+}
+// ending line after each row
+cout << "\n";
+}
+return 0;
+}
