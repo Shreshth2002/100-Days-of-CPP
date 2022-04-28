0e95fc0a07b90fe8dee5d63fbc4acbddce2b93d8
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-28 22:57:11 +0530

    Day-20

---
+++ Arrays/LargestElement.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int a[n];
+
+    cout<<"Enter the size of array"<<endl;
+    cin>>n;
+
+    cout<<"Enter the element number "<<n<<endl;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[n];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<"Elements in the array are : "<<a[n]<<" ";
+    }
+    getch;
+
+    return 0;
+}
---
+++ Arrays/PrintingNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[6]={21,32,45,57,65,78};
+    cout << "Array elements of given array" << endl;
+    for(int i=0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
