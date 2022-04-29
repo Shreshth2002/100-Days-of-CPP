ccd1b9086719f42707037fe4349a2709d2c156db
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-04-29 21:50:53 +0530

    Day-21

--- Arrays/LargestElement.cpp
+++ Arrays/LargestElement.cpp
@@ -1,27 +1,30 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    int a[n];
+int main() {
 
-    cout<<"Enter the size of array"<<endl;
-    cin>>n;
+  int i, n;
+  float arr[100];
 
-    cout<<"Enter the element number "<<n<<endl;
+  cout << "Enter total number of elements(1 to 100): ";
+  cin >> n;
+  cout << endl;
 
-    for(int i=0; i<n; i++)
-    {
-        cin>>a[n];
-    }
+  // Store number entered by the user
+  for(i = 0; i < n; ++i) {
+    cout << "Enter Number " << i + 1 << " : ";
+    cin >> arr[i];
+  }
 
-    for(int i=0; i<n; i++)
-    {
-        cout<<"Elements in the array are : "<<a[n]<<" ";
-    }
-    getch;
+  // Loop to store largest number to arr[0]
+  for(i = 1;i < n; ++i) {
 
-    return 0;
+    // Change < to > if you want to find the smallest element
+    if(arr[0] < arr[i])
+      arr[0] = arr[i];
+  }
+
+  cout << endl << "Largest element = " << arr[0];
+
+  return 0;
 }
