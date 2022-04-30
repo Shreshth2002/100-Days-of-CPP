223bbdcd3da32e52d71bb95280489bfd94cb0db6
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-05-01 03:35:47 +0530

    Day-22

---
+++ Arrays/SumOfEvenElements.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int sum=0;
+int a[6]={4,8,6,7,8,9};
+
+for(int i=0; i<6; i++)
+{
+    if(a[i]%2==0)
+    {
+        sum += a[i];
+    }    
+}
+cout<<sum;
+
+return 0;
+}
