4a0a6f225f5b798d14a70ebeef44724e821c6c17
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-05-03 21:55:09 +0530

    Day-23

---
+++ Arrays/Maximum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int arr[10], n, max;
+
+    cout << "Enter the size of the array : ";
+    cin >> n;
+
+    cout << "Enter the elements of the array : ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+    }
+
+    cout << "Largest element : " << max;
+    
+    return 0;
+}
