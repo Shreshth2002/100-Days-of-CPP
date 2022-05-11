16746366f1db5dbd923f286723dc6a6701fb7a8b
Shreshth2002 <shreshth.khare.221102@gmail.com>
2022-05-11 15:26:06 +0530

    Day-29

---
+++ Pointers/Address.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<<"Enter an integer value: ";
+    cin>>a;
+
+    cout<<"Address of "<<a<<" is "<<&a;
+
+    return 0;
+}
